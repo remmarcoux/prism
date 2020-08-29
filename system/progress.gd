c7f8dd51cf1c2386ba365ee61b89264e75881cf6
nanyx <psykodoomer@gmail.com>
2020-08-29 19:16:35 -0400

    adding convention

--- system/progress.gd
+++ system/progress.gd
@@ -1,16 +1,16 @@
 extends Node
 
 # Declare member variables here
-var  initFile = "res://system/progress.json"
+var initFile = "res://system/progress.json"
 var usrFile = "user://data/progress.json"
 var data = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	load_game()
+	_load_game()
 
 # Load game progression or setup the new one
-func load_game():
+func _load_game():
 	var file = File.new()
 	if not file.file_exists(usrFile):
 		file.open(initFile, File.READ)
@@ -19,25 +19,25 @@ func load_game():
 	
 	data = parse_json(file.get_as_text())
 	file.close()
-	
 
 # Save the game on the client
-func save():
+func _save():
 	var file = File.new()
 	file.open(usrFile, File.WRITE)
 	file.store_line(to_json(data))
 	file.close()
 
 # Re-initialise to default value (start again mofo)
-func resetGame():
+func _resetGame():
 	var file = File.new()
 	file = file.open(initFile, File.READ)
 	data = parse_json(file.get_as_text())
 	file.close()
-	save()
+	_save()
 
 func completeStage(stageID):
 	for i in data:
 		if stageID == data[i].id:
 			data[i].isCompleted = true
+			_save()
 			return
