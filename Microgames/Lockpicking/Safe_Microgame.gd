2997508c7f830f5e97d9e24816ab31116f2b7a96
Francois Boivin <boivin.92@gmail.com>
2020-08-29 21:51:06 -0400

    Microgame manager

--- Microgames/Lockpicking/Safe_Microgame.gd
+++ Microgames/Lockpicking/Safe_Microgame.gd
@@ -2,3 +2,7 @@ extends "res://Microgames/Microgame.gd"
 
 func _ready():
 	$GameContainer/Safe_game.set_process(false)
+
+
+func _on_Safe_game_completed():
+	emit_signal("completed")
--- Microgames/Meta/MinigameBatch.gd
+++ Microgames/Meta/MinigameBatch.gd
@@ -1,4 +1,22 @@
-extends Node2D
+extends Node
 
 export(Array, PackedScene) var gameList : Array
 
+var index = 0
+
+func _ready():
+	load_game()
+
+func load_game():
+	for c in get_children():
+		c.queue_free()
+	var game : microgame = gameList[index].instance()
+	game.connect("completed", self, "game_completed")
+	add_child(game)
+	index += 1
+
+func game_completed():
+	if index == gameList.size():
+		pass #You win, yay!
+	else:
+		load_game()
